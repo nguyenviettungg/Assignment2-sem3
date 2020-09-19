7a431d8a944661ae3731d8e5e386345a126b4c36
nguyenviettungg <nguyentung696969@gmail.com>
2020-09-19 13:06:57 +0700

    nop bai

---
+++ Assignment2-T1907A/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Assignment2_T1907A.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Assignment2_T1907A
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListViewLoaded(object sender, RoutedEventArgs e) {
+            ObservableCollection<Customer> customers = new ObservableCollection<Customer>();
+            customers.Add(new Customer("Tung1", "0123", "Home"));
+            customers.Add(new Customer("Tung2", "01234", "Company"));
+            customers.Add(new Customer("Tung3", "01235", "Home"));
+            customers.Add(new Customer("Tung4", "01236", "Company"));
+            LV.ItemsSource = customers;
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer(FullName.Text, Telephone.Text, TypeContact.Text);
+            LV.Items.Add(c);
+        }
+    }
+}
---
+++ Assignment2-T1907A/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_T1907A.Models
+{
+    class Customer
+    {
+        private string name;
+        private string phone;
+        private string typeContact;
+
+        public Customer(string name, string phone, string typeContact)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.typeContact = typeContact;
+        }
+
+        public string Name { get => name; set=> name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string TypeContact { get => typeContact; set => typeContact = value; }
+    }
+}
